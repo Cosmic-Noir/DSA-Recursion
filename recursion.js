faaf34c01a1e0ed6465dd6852db3c46b112b4f59
Kristen Doucette <cosmicnoir90@gmail.com>
2019-12-12 19:23:47 -0700

    Power Calculator works

--- recursion.js
+++ recursion.js
@@ -1,6 +1,6 @@
 // Recursive function that counts the number of sheep. Should display the nubmer along with the mesage "Another sheep jumps over the fence" until no more sheep are left.
 
-const sheepCounter = function(num) {
+const sheepCounter = num => {
   // base case
   if (num === 0) {
     return "All sheep jumped over the fence";
@@ -10,4 +10,18 @@ const sheepCounter = function(num) {
   return "Another sheep jumps over the fence" + sheepCounter(num - 1);
 };
 
-sheepCounter(3);
+// sheepCounter(3);
+
+// Power Calculator
+
+const powerCalculator = (int, exp) => {
+  // base case
+  if (exp < 0) {
+    console.log("Exponent should be >= 0");
+  }
+
+  // general case
+  console.log(int ** exp);
+};
+
+powerCalculator(10, 2);
